d422bdb303bef73b1cc86257023d2bbfb741081d
Bola Shrif <polashrif199@gmail.com>
2026-06-30 16:32:09 +0300

    Finish Phase 2

--- Admin.cpp
+++ Admin.cpp
@@ -1,12 +1,78 @@
 #include "Admin.h"
-#include <string>
-Admin ::Admin() : Employee()
+#include "FileManager.h"
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+Admin::Admin() : Employee()
 {
 }
 
 Admin::Admin(int id, string name, string password, double salary)
     : Employee(id, name, password, salary)
 {
+}
+
+void Admin::addEmployee(Employee& employee)
+{
+    FileManager fm;
+    fm.addEmployee(employee);
+}
+
+Employee* Admin::searchEmployee(int id)
+{
+    static vector<Employee> employees;
+
+    FileManager fm;
+    employees = fm.getAllEmployees();
+
+    for (int i = 0; i < employees.size(); i++)
+    {
+        if (employees[i].GetEmployeeId() == id)
+        {
+            return &employees[i];
+        }
+    }
+
+    return nullptr;
+}
+
+void Admin::editEmployee(int id, string name, string password, double salary)
+{
+    FileManager fm;
+
+    vector<Employee> employees = fm.getAllEmployees();
+
+    for (int i = 0; i < employees.size(); i++)
+    {
+        if (employees[i].GetEmployeeId() == id)
+        {
+            employees[i].SetEmployeeName(name);
+            employees[i].SetEmployeePassword(password);
+            employees[i].SetEmployeeSalary(salary);
+            break;
+        }
+    }
+
+    fm.removeAllEmploye();
+
+    for (int i = 0; i < employees.size(); i++)
+    {
+        fm.addEmployee(employees[i]);
+    }
+}
+
+void Admin::listEmployee()
+{
+    FileManager fm;
+
+    vector<Employee> employees = fm.getAllEmployees();
+
+    for (int i = 0; i < employees.size(); i++)
+    {
+        employees[i].display();
+    }
 }
 
 void Admin::display()
--- Admin.h
+++ Admin.h
@@ -6,6 +6,12 @@ class Admin : public Employee
 public:
     Admin();
     Admin(int id, string name, string password, double salary);
-    
-    void display()override;
+
+    // Employee Management
+    void addEmployee(Employee& employee);
+    Employee* searchEmployee(int id);
+    void editEmployee(int id, string name, string password, double salary);
+    void listEmployee();
+
+    void display() override;
 };
